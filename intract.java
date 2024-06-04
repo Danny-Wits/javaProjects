e32c43d17cdad13f06f298cf1f43cbb2fb4963f0
Danishwer _ <dannywits12321@gmail.com>
2024-06-05 01:02:07 +0530

    taking and saving users and  authenticating them

--- intract.java
+++ intract.java
@@ -1,3 +1,17 @@
-public class intract {
-    
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class intract extends JFrame {
+    intract(String x){
+        setFocusable(true);
+        setBounds(10, 10, 1000, 600);
+        setLayout(new FlowLayout());
+        setTitle("Data Manager/"+x);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel l= new JLabel("Welcome "+x);
+        add(l);
+        setVisible(true);
+    }
 }
--- login.java
+++ login.java
@@ -1,22 +1,30 @@
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class login implements ActionListener {
     private JFrame window;
     private JPanel containerH, containerC;
-    private JLabel title, nameText;
-    private JTextField nameInput;
+    private JLabel title, nameText, passwordText;
+    private JTextField nameInput, passwordInput;
     private JButton submit;
+    private ArrayList<User>users=new ArrayList<>();
+    private final String PATH ="list";
 
     login() {
         // !Window Setup
@@ -52,26 +60,99 @@ public class login implements ActionListener {
         containerC = new JPanel();
         containerC.setFocusable(true);
         containerC.setVisible(true);
-        containerC.setLayout(new GridLayout(20, 2));
+        containerC.setLayout(new GridLayout(10, 2));
 
         // setting inputs
+        // Name
         nameText = new JLabel("Name:");
         containerC.add(nameText);
         nameInput = new JTextField();
         containerC.add(nameInput);
+
+        // Password
+        passwordText = new JLabel("password:");
+        containerC.add(passwordText);
+        passwordInput = new JTextField();
+        containerC.add(passwordInput);
 
         // setting buttons
         submit = new JButton();
         submit.setText("Click To Submit");
         submit.addActionListener(this);
         containerC.add(submit);
-
+       
         window.add(containerC, BorderLayout.CENTER);
         window.setVisible(true);
+        //loading saved files
+        load();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.err.println(nameInput.getText());
+        
+        User newUser=new User(nameInput.getText(), passwordInput.getText());
+        //Name Check 
+        boolean flag=false;
+        User found=null;
+        for (User u : users) {
+           if(newUser.matchName(u)){
+            show("User Found");
+            flag = true;
+            found=u;
+            break;
+           }
+        }
+        if (flag){
+        //Password Check
+        if (newUser.matchPassword(found)) {
+            new intract(newUser.getName());
+            window.dispose();
+            return;
+        }else{
+            show("Password does not Match!");
+         }
+        }else{
+            users.add(newUser);
+            save();
+        }
+       
     }
+    void load(){
+         //loading Users
+         File f= new File(PATH);
+         if(f.exists()){
+         //loading from storage
+         try{
+         FileInputStream fIn = new FileInputStream(PATH);
+         ObjectInputStream oIN = new ObjectInputStream(fIn);
+         users=(ArrayList<User>)oIN.readObject();
+         oIN.close();
+         fIn.close();
+         }catch(Exception ex){
+             System.err.println("x");
+             ex.printStackTrace();
+         }finally{
+             for (User u : users) {
+                 System.out.println(u.getName());
+             }
+         }
+         }
+    }
+    void show(String x){
+        JOptionPane.showMessageDialog(window,x,"INFO",JOptionPane.INFORMATION_MESSAGE);
+    }
+    void save(){
+       //Saving User
+       try{
+       FileOutputStream fOut = new FileOutputStream(PATH);
+       ObjectOutputStream oOut = new ObjectOutputStream(fOut);
+        oOut.writeObject(users);
+        oOut.close();
+        fOut.close();
+       }catch(Exception ex){
+        System.err.println("yy");
+           ex.printStackTrace();
+       }  
+    }
+
 }
