6d6422da01fc46809dc111d9125c476f38716aa7
Danishwer _ <dannywits12321@gmail.com>
2024-06-04 16:10:24 +0530

    window

--- first.java
+++ first.java
@@ -1,10 +1,21 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.plaf.FontUIResource;
+
 /**
  * first
  */
 public class first {
 
      public static void main(String[] args) {
-        int x=10;
-        System.out.println(x);
-     }
+        JFrame window = new JFrame();
+        window.setVisible(true);
+        window.setSize(500,500);
+        window.setTitle("First");
+        window.setLayout(null);
+        JLabel text = new JLabel("Welcome");
+        text.setFont(new FontUIResource("DIGIFACE", 0, 50));
+        text.setBounds(10,10,400,100);
+        window.add(text);
+        }
 }
